d648093f2fe6a5953d7b6b72f35fceea68c3b9f9
ddgarrett <doug@anetsc.com>
2021-11-26 07:21:02 -0800

    Evaluate "-" Prefix

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -50,6 +50,8 @@ func evalPrefixExpression(operator string, right object.Object) object.Object {
 	switch operator {
 	case "!":
 		return evalBangOperatorExpression(right)
+	case "-":
+		return evalMinusPrefixOperatorExpression(right)
 	default:
 		return NULL
 	}
@@ -73,4 +75,13 @@ func evalBangOperatorExpression(right object.Object) object.Object {
 		// fmt.Print("  - evalBangOperatorExpression default case\n")
 		return FALSE
 	}
+}
+
+func evalMinusPrefixOperatorExpression(right object.Object) object.Object {
+	if right.Type() != object.INTEGER_OBJ {
+		return NULL
+	}
+
+	value := right.(*object.Integer).Value
+	return &object.Integer{Value: -value}
 }
