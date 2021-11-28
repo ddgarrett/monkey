a32d1edd762c6af2cb4de120ae69de7074bb04c6
ddgarrett <doug@anetsc.com>
2021-11-28 05:55:33 -0800

    Add String Concat Operator
    Also is a hook for logical string comparisons

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -213,6 +213,10 @@ func TestErrorHandling(t *testing.T) {
 			"unknown operator: BOOLEAN + BOOLEAN",
 		},
 		{
+			`"Hello" - "World"`,
+			"unknown operator: STRING - STRING",
+		},
+		{
 			`
 			if (10 > 1) {
 				if (10 > 1) {
@@ -283,6 +287,20 @@ func TestFunctionApplication(t *testing.T) {
 
 func TestStringLiteral(t *testing.T) {
 	input := `"Hello World!"`
+
+	evaluated := testEval(input)
+	str, ok := evaluated.(*object.String)
+	if !ok {
+		t.Fatalf("object is not String. got=%T (%+v)", evaluated, evaluated)
+	}
+
+	if str.Value != "Hello World!" {
+		t.Errorf("String has wrong value. got=%q", str.Value)
+	}
+}
+
+func TestStringConcatenation(t *testing.T) {
+	input := `"Hello" + " " + "World!"`
 
 	evaluated := testEval(input)
 	str, ok := evaluated.(*object.String)
