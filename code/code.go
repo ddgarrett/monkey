58ff14fbb5f269948bdd244327608f62815ae9f9
ddgarrett <doug@anetsc.com>
2021-12-15 07:57:21 -0800

    Index Array and Hash Literals

--- code/code.go
+++ code/code.go
@@ -38,6 +38,7 @@ const (
 
 	OpArray
 	OpHash
+	OpIndex
 )
 
 type Definition struct {
@@ -73,6 +74,7 @@ var definitions = map[Opcode]*Definition{
 
 	OpArray: {"OpArray", []int{2}},
 	OpHash:  {"OpHash", []int{2}},
+	OpIndex: {"OpIndex", []int{}},
 }
 
 func Lookup(op byte) (*Definition, error) {
