3d750c4d1f43bd18869552df4ba5625920755d30
ddgarrett <doug@anetsc.com>
2021-12-21 09:33:29 -0800

    Refactor Remaining Builtins
    functions: first, last, rest, push

--- object/builtins.go
+++ object/builtins.go
@@ -17,6 +17,10 @@ var Builtins = []struct {
 }{
 	{"len", &Builtin{Fn: _len}},
 	{"puts", &Builtin{Fn: _puts}},
+	{"first", &Builtin{Fn: _first}},
+	{"last", &Builtin{Fn: _last}},
+	{"rest", &Builtin{Fn: _rest}},
+	{"push", &Builtin{Fn: _push}},
 }
 
 func newError(format string, a ...interface{}) *Error {
@@ -46,4 +50,82 @@ func _puts(args ...Object) Object {
 	}
 
 	return nil
+}
+
+func _first(args ...Object) Object {
+	if len(args) != 1 {
+		return newError("wrong number of arguments. got=%d, want=1",
+			len(args))
+	}
+	if args[0].Type() != ARRAY_OBJ {
+		return newError("argument to `first` must be ARRAY, got %s",
+			args[0].Type())
+	}
+
+	arr := args[0].(*Array)
+	if len(arr.Elements) > 0 {
+		return arr.Elements[0]
+	}
+
+	return nil
+}
+
+func _last(args ...Object) Object {
+	if len(args) != 1 {
+		return newError("wrong number of arguments. got=%d, want=1",
+			len(args))
+	}
+	if args[0].Type() != ARRAY_OBJ {
+		return newError("argument to `last` must be ARRAY, got %s",
+			args[0].Type())
+	}
+
+	arr := args[0].(*Array)
+	length := len(arr.Elements)
+	if length > 0 {
+		return arr.Elements[length-1]
+	}
+
+	return nil
+}
+
+func _rest(args ...Object) Object {
+	if len(args) != 1 {
+		return newError("wrong number of arguments. got=%d, want=1",
+			len(args))
+	}
+	if args[0].Type() != ARRAY_OBJ {
+		return newError("argument to `rest` must be ARRAY, got %s",
+			args[0].Type())
+	}
+
+	arr := args[0].(*Array)
+	length := len(arr.Elements)
+	if length > 0 {
+		newElements := make([]Object, length-1)
+		copy(newElements, arr.Elements[1:length])
+		return &Array{Elements: newElements}
+	}
+
+	return nil
+}
+
+func _push(args ...Object) Object {
+	if len(args) != 2 {
+		return newError("wrong number of arguments. got=%d, want=2",
+			len(args))
+	}
+	if args[0].Type() != ARRAY_OBJ {
+		return newError("argument to `push` must be ARRAY, got %s",
+			args[0].Type())
+	}
+
+	arr := args[0].(*Array)
+	length := len(arr.Elements)
+
+	newElements := make([]Object, length+1)
+	copy(newElements, arr.Elements)
+	newElements[length] = args[1]
+
+	return &Array{Elements: newElements}
 }
