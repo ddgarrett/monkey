bd5baa211790a7650c5efdc404025a3d384aea8c
ddgarrett <doug@anetsc.com>
2021-12-31 14:18:52 -0800

    Add Test For Assignment Without Let Literal

--- compiler/global_test.go
+++ compiler/global_test.go
@@ -5,6 +5,54 @@ import (
 	"testing"
 )
 
+func TestGlobalLetStatementsWithoutLet(t *testing.T) {
+	tests := []compilerTestCase{
+		{
+			input: `
+            one = 1;
+            two = 2;
+            `,
+			expectedConstants: []interface{}{1, 2},
+			expectedInstructions: []code.Instructions{
+				code.Make(code.OpConstant, 0),
+				code.Make(code.OpSetGlobal, 0),
+				code.Make(code.OpConstant, 1),
+				code.Make(code.OpSetGlobal, 1),
+			},
+		},
+		{
+			input: `
+            one = 1;
+            one;
+            `,
+			expectedConstants: []interface{}{1},
+			expectedInstructions: []code.Instructions{
+				code.Make(code.OpConstant, 0),
+				code.Make(code.OpSetGlobal, 0),
+				code.Make(code.OpGetGlobal, 0),
+				code.Make(code.OpPop),
+			},
+		},
+		{
+			input: `
+            one = 1;
+            two = one;
+            two;
+            `,
+			expectedConstants: []interface{}{1},
+			expectedInstructions: []code.Instructions{
+				code.Make(code.OpConstant, 0),
+				code.Make(code.OpSetGlobal, 0),
+				code.Make(code.OpGetGlobal, 0),
+				code.Make(code.OpSetGlobal, 1),
+				code.Make(code.OpGetGlobal, 1),
+				code.Make(code.OpPop),
+			},
+		},
+	}
+
+	runCompilerTests(t, tests)
+}
 func TestGlobalLetStatements(t *testing.T) {
 	tests := []compilerTestCase{
 		{
